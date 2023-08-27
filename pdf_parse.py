d240e542748d164b62e816230e8c9890653b92d7
Terrasse <terrasse@qq.com>
2023-08-27 21:44:51 +0800

    add bibitem collecting

--- pdf_parse.py
+++ pdf_parse.py
@@ -1,18 +1,24 @@
 import PyPDF2
-from PyPDF2.generic import AnnotationBuilder
+from PyPDF2.generic import AnnotationBuilder, Destination
 import numpy as np
 # from PIL import Image, ImageDraw, ImageFont
 from py_pdf_parser import loaders
 from shapely import geometry
+import sys
 
 fname = "pdf/2201.02915.pdf"
+# fname = "pdf/2307.00059.pdf"
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
 pdf = PyPDF2.PdfReader(fname)
 
 writer = PyPDF2.PdfWriter()
 
 mark_links = True
-mark_all_texts = True
+mark_bibitems = True
+mark_all_texts = False
 mark_link_texts = False
+mark_all_bibitems = True
 
 # {'/Type': '/Annot', '/Subtype': '/Link', '/A': {'/Type': '/Action', '/S': '/URI', '/URI': 'https://www.aminer.cn/'}, '/Border': [0, 0, 0], '/C': [0, 1, 1], '/H': '/I', '/Rect': [58.27, 98.089, 128.09, 108.493]}
 # {'/Type': '/Annot', '/Subtype': '/Link', '/A': {'/D': 'cite.Hirsch2005H-index', '/S': '/GoTo'}, '/Border': [0, 0, 0], '/C': [0, 1, 0], '/H': '/I', '/Rect': [459.051, 392.88, 469.383, 400.508]}
@@ -26,6 +32,10 @@ mark_link_texts = False
 #     x = x / y * fontSize
 #     return x
 
+def dot_in_rect(rect, dot, dis=0.0):
+    rect_ext = [rect[0] - dis, rect[1] - dis, rect[2] + dis, rect[3] + dis]
+    return geometry.box(*rect_ext).contains(geometry.Point(*dot))
+
 def contains(big_rect, small_rect, threshold=1.0):
     # big_rect = [x0, y0, x1, y1]
     # small_rect = [x0, y0, x1, y1]
@@ -36,18 +46,50 @@ def contains(big_rect, small_rect, threshold=1.0):
 class Annotation:
     def __init__(self, obj, page, rect, to, text=None):
         self.obj = obj
-        self.page = page
+        self.page = page # the annotation is on which page
         self.rect = rect
         self.to = to
         self.text = text
     def bind(self, text: str):
         self.text = text
 
+class Bibitem:
+    def __init__(self, obj: Destination, page: int) -> None:
+        self.obj = obj
+        self.title = obj.title
+        assert obj.left and obj.top
+        self.x = obj.left.as_numeric()
+        self.y = obj.top.as_numeric()
+        self.pos = (self.x, self.y)
+        self.page = page
+    def __repr__(self) -> str:
+        return f"<Bibitem: {self.title} on page {self.page} ({self.x}, {self.y})>"
+
 pdf_parser = loaders.load_file(fname)
+for page in pdf.pages:
+    writer.add_page(page)
+
+page_id2idx: dict[int, int] = {}
+for idx, page in enumerate(pdf.pages):
+    assert page.indirect_ref
+    page_id2idx[page.indirect_ref.idnum] = idx
+
+bibitems: list[Bibitem] = []
+for name, obj in pdf.named_destinations.items():
+    if obj['/Type'] != '/XYZ':
+        continue # skip non-XYZ destinations
+    bibitems.append(Bibitem(obj, page_id2idx[obj.page.idnum]))
+
+if mark_all_bibitems:
+    for bib in bibitems:
+        mark = AnnotationBuilder.rectangle(
+            rect=(bib.x, bib.y, bib.x + 5, bib.y + 5),
+        )
+        writer.add_annotation(page_number=bib.page, annotation=mark)
 
 links = []
 for idx, page in enumerate(pdf.pages):
-    writer.add_page(page)
+    # find links
     page_links = []
     assert page.annotations
     for annot in page.annotations:
@@ -61,6 +103,7 @@ for idx, page in enumerate(pdf.pages):
                 writer.add_annotation(page_number=idx, annotation=mark)
     links.extend(page_links)
 
+    # match links to texts
     page_parser = pdf_parser.get_page(idx + 1)
     for element in page_parser.elements:
         bbox = element.bounding_box
@@ -76,6 +119,16 @@ for idx, page in enumerate(pdf.pages):
                 link.bind(element.text())
                 context = element.text()[: 200].replace('\n', ' ').strip()
                 print("Found link:", link.to, "on page", link.page, "with context:", f"`{context}`")
+        
+        for bib in bibitems:
+            if idx == bib.page and dot_in_rect(rect, bib.pos, 1.0):
+                context = element.text()[: 200].replace('\n', ' ').strip()
+                print("Found bibitem:", bib.title, "on page", bib.page, "with context:", f"`{context}`")
+                if mark_bibitems:
+                    mark = AnnotationBuilder.rectangle(
+                        rect=rect,
+                    )
+                    writer.add_annotation(page_number=idx, annotation=mark)
     # def visitor(text, cm, tm, fontDict, fontSize):
     #     pos = np.array([0., 0., 1.])
     #     cmat = np.array([
@@ -116,10 +169,10 @@ for idx, page in enumerate(pdf.pages):
     # page.extract_text(visitor_text=visitor)
 print()
 
-from pdfminer.high_level import extract_pages
-for page_layout in extract_pages(fname):
-    for element in page_layout:
-        print(element)
+# from pdfminer.high_level import extract_pages
+# for page_layout in extract_pages(fname):
+#     for element in page_layout:
+#         print(element)
 
 
 
